3be26b1eb1c0517725afc6d3a47735ff3d135324
Muhammad Kresna Hari Mufti <141144099+noya120@users.noreply.github.com>
2025-12-11 15:59:11 +0700

    Update loginuser-spiketest.js

--- loginuser-spiketest.js
+++ loginuser-spiketest.js
@@ -3,10 +3,10 @@ import { check } from "k6";
 
 export let options = {
     stages: [
-        { duration: "5s", target: 10 },     // pemanasan
-        { duration: "1s", target: 200 },    // LONJAKAN MENDADAK
-        { duration: "20s", target: 200 },   // tahan puncak
-        { duration: "5s", target: 0 },      // kembali ke normal
+        { duration: "5s", target: 10 },    
+        { duration: "1s", target: 200 },  
+        { duration: "20s", target: 200 },   
+        { duration: "5s", target: 0 },      
     ],
 };
 
