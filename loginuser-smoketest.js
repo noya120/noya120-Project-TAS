af909580e986570410970b61555540a767257d71
Muhammad Kresna Hari Mufti <141144099+noya120@users.noreply.github.com>
2025-12-11 15:58:55 +0700

    Update loginuser-smoketest.js

--- loginuser-smoketest.js
+++ loginuser-smoketest.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { check } from "k6";
 
 export let options = {
-    vus: 1,           // smoke test = 1 user saja
+    vus: 1,          
     duration: "20s",
 };
 
